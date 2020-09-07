1b0dfeaef9f25a0f20e71a388a7ce3a005177ef1
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-07 03:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ class Cat {
     return `${this.name} says meow!`
   }
 }
+
+class Dog {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    return `${this.name} says woof!`
+  }
+}
+
+class Cat {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    return `${this.name} says meow!`
+  }
+}
